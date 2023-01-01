72f8ce9a221a6beeacda0bce27006819b6afa110
Rafiere <rafapgbr@gmail.com>
2023-01-01 14:26:15 -0300

    feat: adding code from lesson 01.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,15 @@
 import { Body, Controller, Post } from "@nestjs/common";
+import { UserService } from "./user.service";
 
+/* A controller class should only be responsible for handling the HTTP protocol. */
+
+/* A controller should only know that the service will create a user, that is, it should not know how a user will be created by the service layer. */
 @Controller("users")
 export class UserController {
-  private users = [];
+  private userService = new UserService();
 
   @Post()
   public create(@Body() user) {
-    this.users.push(user);
-
-    return user;
+    return this.userService.create(user);
   }
 }
