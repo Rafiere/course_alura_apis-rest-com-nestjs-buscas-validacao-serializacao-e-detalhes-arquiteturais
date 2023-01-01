0215458ab11a29803ab71b4e7b422d191c85dfc1
Rafiere <rafapgbr@gmail.com>
2023-01-01 15:59:46 -0300

    feat: adding validations to the user entity.

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,8 +1,20 @@
+import { IsNotEmpty, IsString, IsEmail } from "class-validator";
+
 export class User {
   id: number;
+
+  /* The "class-validator" package adds a lot of decorators that abstract the logic of various validations. */
+  @IsNotEmpty()
+  @IsString()
   nickname: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsNotEmpty({ message: "The 'fullname' is a required field." })
   fullName: string;
   registeredAt: Date;
 }
