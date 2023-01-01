315bf2b871bbe845d5f1aabb4e78fda8bd560a34
Rafiere <rafapgbr@gmail.com>
2023-01-01 15:49:47 -0300

    feat: adding commments to the main module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { UserController } from "./user/user.controller";
+import { UserModule } from "./user/user.module";
+import { UserService } from "./user/user.service";
 
+/* In the "AppModule", we must import all application modules. */
 @Module({
-  imports: [],
-  controllers: [UserController],
+  imports: [UserModule],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
